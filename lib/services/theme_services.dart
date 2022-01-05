fb2b62a5f247b06585d82d932547a9decebd1fc9
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-01-05 23:28:32 +0300

    function to switch themes dynamically

--- lib/services/theme_services.dart
+++ lib/services/theme_services.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:get/get.dart';
 
 class ThemeServices{
   final _box = GetStorage();
@@ -9,4 +10,9 @@ class ThemeServices{
 
   ThemeMode get theme => _loadThemeFromBox()
       ?ThemeMode.dark:ThemeMode.light;
+
+  void switchTheme(){
+    Get.changeThemeMode(_loadThemeFromBox()
+        ?ThemeMode.light:ThemeMode.dark);
+  }
 }
