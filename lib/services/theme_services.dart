6bd260d9ae6fa9bb4dc0c6f146a2a85cd60ccf5f
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-01-05 23:21:33 +0300

    adding function

--- lib/services/theme_services.dart
+++ lib/services/theme_services.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 
 class ThemeServices{
@@ -5,4 +6,7 @@ class ThemeServices{
   final _key = "isDarkMode";
 
   bool _loadThemeFromBox() => _box.read(_key)??false;
+
+  ThemeMode get theme => _loadThemeFromBox()
+      ?ThemeMode.dark:ThemeMode.light;
 }
