87b058282b3c701fe5df52b79973e1adb173c77f
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-01-20 21:46:16 +0300

    NotifyHelper Class

--- lib/services/notification_services.dart
+++ lib/services/notification_services.dart
@@ -1,4 +1,30 @@
 class NotifyHelper{
+  FlutterLocalNotificationsPlugin
+  flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin(); //
+
+  initializeNotification() async {
+    //tz.initializeTimeZones();
+    final IOSInitializationSettings initializationSettingsIOS =
+    IOSInitializationSettings(
+        requestSoundPermission: false,
+        requestBadgePermission: false,
+        requestAlertPermission: false,
+        onDidReceiveLocalNotification: onDidReceiveLocalNotification
+    );
 
 
+    final Android InitializationSettings initializationSettingsAndroid =
+        Android InitializationSettings("appicon);
+
+      final InitializationSettings initializationSettings =
+      InitializationSettings(
+      iOS: initializationSettingsIOS,
+      android:initializationSettingsAndroid,
+    );
+    await flutterLocalNotificationsPlugin.initialize(
+        initializationSettings,
+        onSelectNotification: selectNotification);
+
+  }
 }
