556643360f4d8bd58ff180207b3044137c9fd58b
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-01-20 20:28:15 +0300

    saving theme function

--- lib/services/theme_services.dart
+++ lib/services/theme_services.dart
@@ -6,6 +6,8 @@ class ThemeServices{
   final _box = GetStorage();
   final _key = "isDarkMode";
 
+  _saveThemeToBox(bool isDarkMode) => _box.write(_key, isDarkMode);
+
   bool _loadThemeFromBox() => _box.read(_key)??false;
 
   ThemeMode get theme => _loadThemeFromBox()
@@ -14,5 +16,7 @@ class ThemeServices{
   void switchTheme(){
     Get.changeThemeMode(_loadThemeFromBox()
         ?ThemeMode.light:ThemeMode.dark);
+
+
   }
 }
